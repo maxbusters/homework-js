f3764a129b558643165be5e05de91d83aa579893
Max Skvortsov <maxbusters1387@gmail.com>
2022-08-26 16:31:05 +0300

    add mozJpeg

--- 15/gulpfile.js
+++ 15/gulpfile.js
@@ -1,10 +1,13 @@
 "use strict";
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
 
 async function optimizeImg(){
 	gulp.src('./assets/images/*')
-		.pipe(imagemin())
+		.pipe(imagemin([
+            imageminMozjpeg({quality: 50, progressive: true}),
+        ]))
 		.pipe(gulp.dest('./assets/images_opt/'))
 };
 
