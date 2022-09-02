cfb3628a8223b19f575668ef2af4db62e862e7c8
Max Skvortsov <maxbusters1387@gmail.com>
2022-09-02 15:30:02 +0300

    deploy

---
+++ 18/18/dist/js/about.a21d5db7.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunk_18"]=self["webpackChunk_18"]||[]).push([[443],{5399:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var s=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],a=u(1001),l={},r=(0,a.Z)(l,s,e,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.a21d5db7.js.map
