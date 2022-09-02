a314c9450eedbe65b9123e3cd770e07691283441
Max Skvortsov <maxbusters1387@gmail.com>
2022-09-02 16:14:40 +0300

    add vue-cli project to repo

---
+++ 18/18/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Main/>
+  </div>
+</template>
+<script>
+import Main from "@/components/Main.vue"
+export default {
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./src/assets/css/style.scss";
+</style>
---
+++ 18/18/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    v-if="$index === $index"
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
